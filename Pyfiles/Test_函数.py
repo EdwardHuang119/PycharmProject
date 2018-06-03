08dd51a05bbce5b9bc2c9f6e0304ca1cc02d1777
Edward Huang <26125482@qq.com>
2018-06-03 22:54:25 +0800

    Try tushare

--- Pyfiles/Test_函数.py
+++ Pyfiles/Test_函数.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
-def power(x, n):
-    s = 1
-    while n > 0:
-        n = n - 1
-        s = s * x
-    return s
-print(power(5,3))
-# def power(x):
-#     return x * x
-# print(power(5))
+import time
+# def power(x, n):
+#     s = 1
+#     while n > 0:
+#         n = n - 1
+#         s = s * x
+#     return s
+# print(power(5,3))
+# # def power(x):
+# #     return x * x
+# # print(power(5))
+#
+# l =  [x * x for x in range(1, 11) if x % 2  == 1]
+# print(l)
+a = '20150101'
+t=time.strptime(a,'%Y%m%d')
+time =time.strftime('%Y-%m-%d',t)
+print(t,time)
 
-l =  [x * x for x in range(1, 11) if x % 2  == 1]
-print(l)
+
